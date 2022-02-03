56760541c2200b7bf4f35e0aca03e0cbabb33373
lib <lib.martinito@pm.me>
2022-02-03 16:29:34 +0800

    Added functions for converting to camelcase and to titlecase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './styles.css';
+import { convertToCamelCase, convertToTitleCase } from './logic.js';
+
+let add = document.querySelector('.add-todo');
+add.addEventListener('click', () => {
+    convertToCamelCase('All tasks');
+});
+console.log(convertToCamelCase("All tasks"));
+console.log(convertToTitleCase('allTasks'));
--- src/logic.js
+++ src/logic.js
@@ -5,3 +5,42 @@ const toDo = function(title, description, dueDate, priority) {
   this.priority = priority;
 };
 
+// A function that converts to camelcase
+
+const convertToCamelCase = (str) => {
+  let strArr = str.split('');
+  let camelCaseStrArray = [];
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i] != ' ') {
+      if (strArr[i] == 0) {
+        camelCaseStrArray.push(strArr[i].toLowerCase());
+      } else if (strArr[i - 1] == ' ') {
+        camelCaseStrArray.push(strArr[i].toUpperCase());
+      } else {
+        camelCaseStrArray.push(strArr[i].toLowerCase());
+      };
+    };
+  };
+  return camelCaseStrArray.join('');
+};
+
+// A function that converts to titlecase
+
+const convertToTitleCase = (str) => {
+  let strArr = str.split('');
+  let titleCaseStrArr = [];
+  for (let i = 0; i < strArr.length; i++) {
+    if (i == 0) {
+      titleCaseStrArr.push(strArr[i].toUpperCase());
+    } else if (strArr[i] == strArr[i].toUpperCase()) {
+      titleCaseStrArr.push(' ');
+      titleCaseStrArr.push(strArr[i]);
+    } else {
+      titleCaseStrArr.push(strArr[i]);
+    };
+  };
+  return titleCaseStrArr.join('');
+};
+
+export{convertToCamelCase, convertToTitleCase}
+
